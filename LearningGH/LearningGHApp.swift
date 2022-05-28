affee926494d032e7ab3f4be1f8a41f7985b8445
MichielN <Asmilo@home.nl>
2022-05-28 20:41:22 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Michiel Nooij on 28/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
